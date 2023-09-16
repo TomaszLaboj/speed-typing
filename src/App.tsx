46f50ecd42bafc224ff4d6cf61ab465e0b2474d2
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-16 23:44:22 +0100

    add generate words fn plus display timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./input.css";
-function App() {
-  return (
-    <div>
-      <p className="text-4xl text-center text-blue-500">Hello world!</p>
-    </div>
-  );
-}
+import { faker } from "@faker-js/faker";
 
+const words = faker.word.words(20);
+console.log(words);
+const App = () => {
+  return (
+    <>
+      <CountdownTimer timeLeft={30} />
+      <GeneratedWords words={words} />
+    </>
+  );
+};
+
+const GeneratedWords = ({ words }: { words: string }) => {
+  return <div className="text-4xl text-center text-slate-500">{words}</div>;
+};
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
+};
 export default App;
