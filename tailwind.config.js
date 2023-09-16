911bfd5b52a38325fbc42e455647d7f1170e51ea
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-16 23:44:03 +0100

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export const content = ["./src/**/*.{html,js,ts,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: colors.yellow,
+    },
+  },
 };
 export const plugins = [];
